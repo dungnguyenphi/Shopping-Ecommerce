2276e94bc35fc62bd898cf7d7469fe3df033755a
Dung Nguyen <dungnguyenphi94@gmail.com>
2022-01-20 19:42:37 +0700

    fix bug

--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -24,4 +24,3 @@ export const ShopPage = () => {
     </div>
   );
 };
-export default withSpinner(ShopPage);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import { HomePage } from "./HomePage/HomePage";
 import { HatsPage } from "./HatsPage/HatsPage";
-import ShopPage from "./ShopPage/ShopPage";
+import { ShopPage } from "./ShopPage/ShopPage";
 import { SignIn } from "./SignIn/SignIn";
 import { Checkout } from "./Checkout/Checkout";
 import { Collection } from "./Collection/Collection";
